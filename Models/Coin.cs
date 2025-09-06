bf5b5fe3fc5cc4e3b55702ec59d76bed6770522b
Bibia-07 <bibica.pc2020@gmail.com>
2025-09-06 11:53:31 -0300

    ..

---
+++ Models/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTeste.Models
+{
+    public class Coin
+    {
+        public string Lado { get; set; } = string.Empty;
+
+        public string Jogar()
+        {
+            int LadoSorteado = new Random().Next(2);
+            Lado = (LadoSorteado == 0) ? "Cara" : "Coroa"; 
+            return Lado;
+        }
+
+        public string Jogar(string LadoEscolhido)
+        {
+            int LadoSorteado = new Random().Next(2);
+            Lado = (LadoSorteado == 0) ? "Cara" : "Coroa";
+
+            string Resultado = (Lado == LadoEscolhido) ?
+                $"Parabéns, Você pediu {LadoEscolhido} e deu {Lado}" :
+                $"Que pena, você {LadoEscolhido} e deu {Lado}";
+            
+            return Resultado;
+        }
+    }
+}
